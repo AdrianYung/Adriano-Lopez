7e1ae7ba4d8f41349cb2017af62f608abd4cfbae
AdrianYung <144393466+AdrianYung@users.noreply.github.com>
2024-03-03 23:51:13 +0000

    ultimos cambios

--- script/pintar.js
+++ script/pintar.js
@@ -1,65 +1,44 @@
 var pantalla = document.querySelector('canvas');
-        var pincel = pantalla.getContext('2d');
+var pincel = pantalla.getContext('2d');
 
-        var colores = ["white","black","Yellow",
-                        "blue","red","orange",
-                        "green","skyblue","darkgreen",
-                        "grey"];
-        var tamahoCuadrado = 30;
-        var j = 0;
+var colores = ["white", "black", "yellow", "blue", "red", "orange", "green", "skyblue", "darkgreen", "grey"];
+var tamanoCuadrado = 30;
+var j = 0;
 
-        pincel.fillStyle = 'grey';
-        pincel.fillRect(0, 0, 400, 400);
+pincel.fillStyle = 'grey';
+pincel.fillRect(0, 0, 400, 400);
 
-        crearPaleta();
+crearPaleta();
 
-        function crearPaleta() {
-            let xMover = 5;
-            for(let i = 0; i < colores.length; i++) {
-                pincel.fillStyle = colores[i];
-                pincel.fillRect(xMover,5,tamahoCuadrado,tamahoCuadrado);
-                pincel.stroke = "black";
-                pincel.strokeRect(xMover,5,tamahoCuadrado,tamahoCuadrado);
-                xMover = xMover + 30;
-            }
-        }
-
-        function seleccionarColor(evento) {
-            let x = evento.pageX-pantalla.offsetLeft;
-            let y = evento.pageY-pantalla.offsetTop;
-
-            if(x < tamahoCuadrado*colores.length+5 && x > 5
-            && y < tamahoCuadrado+5 && y > 5) {
-                for(let i = 0; i < colores.length; i++) {
-                    if(x < tamahoCuadrado*(i+1)+5 && x > 5
-                    && y < tamahoCuadrado+5 && y > 5) {
-                        j = i;
-                        break;
-                    }
-                }
-            }
-        }
-
-        var puedoDibujar = false;
+function crearPaleta() {
+    let paleta = document.getElementById('paleta');
+    
+    for (let i = 0; i < colores.length; i++) {
+        let colorDiv = document.createElement('div');
+        colorDiv.style.backgroundColor = colores[i];
+        colorDiv.addEventListener('click', function () {
+            j = i;
+        });
+        paleta.appendChild(colorDiv);
+    }
+}
 
 function dibujarCirculo(evento) {
-    if (puedoDibujar) {
-        var x, y;
+    var x, y;
 
-        if (evento.type === 'mousemove') {
-            x = evento.pageX - pantalla.offsetLeft;
-            y = evento.pageY - pantalla.offsetTop;
-        } else if (evento.type === 'touchmove') {
-            x = evento.touches[0].clientX - pantalla.offsetLeft;
-            y = evento.touches[0].clientY - pantalla.offsetTop;
-        }
+    if (evento.type === 'mousemove') {
+        x = evento.pageX - pantalla.offsetLeft;
+        y = evento.pageY - pantalla.offsetTop;
+    } else if (evento.type === 'touchmove') {
+        x = evento.touches[0].clientX - pantalla.offsetLeft;
+        y = evento.touches[0].clientY - pantalla.offsetTop;
+    }
 
-        if (y > tamahoCuadrado + 10) {
-            pincel.fillStyle = colores[j];
-            pincel.beginPath();
-            pincel.arc(x, y, 5, 0, 2 * Math.PI);
-            pincel.fill();
-        }
+    if (y > tamanoCuadrado + 10) {
+        pincel.fillStyle = colores[j];
+        pincel.beginPath();
+        pincel.arc(x, y, 5, 0, 2 * Math.PI);
+        pincel.fill();
     }
 }
 
@@ -67,11 +46,13 @@ pantalla.addEventListener('mousemove', dibujarCirculo);
 pantalla.addEventListener('touchmove', dibujarCirculo);
 
 function habilitarDibujar() {
-    puedoDibujar = true;
+    pantalla.addEventListener('mousemove', dibujarCirculo);
+    pantalla.addEventListener('touchmove', dibujarCirculo);
 }
 
 function deshabilitarDibujar() {
-    puedoDibujar = false;
+    pantalla.removeEventListener('mousemove', dibujarCirculo);
+    pantalla.removeEventListener('touchmove', dibujarCirculo);
 }
 
 pantalla.addEventListener('mousedown', habilitarDibujar);
@@ -79,5 +60,3 @@ pantalla.addEventListener('touchstart', habilitarDibujar);
 
 pantalla.addEventListener('mouseup', deshabilitarDibujar);
 pantalla.addEventListener('touchend', deshabilitarDibujar);
-
-pantalla.onclick = seleccionarColor;
